cfaa840c6c92c1bc3c48b9ae51f5998724141a36
Tencona <connertennery@gmail.com>
2019-02-05 03:33:56 -0600

    Update background orange
    Made it a little lighter

--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -1,102 +1,102 @@
 <template>
-  <div class="diary">
-    <h1>Diary</h1>
-    <div class="entriesWrapper">
-      <div class="entries">
-        <DiaryEntry
-          class="entry"
-          v-for="(entry, i) in entries"
-          :key="i"
-          v-bind:entry.sync="entry"
-          :index="i"
-          @toggle-state="entryToggledState"
-          @delete-entry="deleteEntry"
-        />
-      </div>
-    </div>
-  </div>
+	<div class="diary">
+		<h1>Diary</h1>
+		<div class="entriesWrapper">
+			<div class="entries">
+				<DiaryEntry
+					class="entry"
+					v-for="(entry, i) in entries"
+					:key="i"
+					v-bind:entry.sync="entry"
+					:index="i"
+					@toggle-state="entryToggledState"
+					@delete-entry="deleteEntry"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 window.onbeforeunload = function(e) {
-  return unsavedChanges
-    ? "Are you sure? Unsaved changes will be lost"
-    : undefined;
+	return unsavedChanges
+		? "Are you sure? Unsaved changes will be lost"
+		: undefined;
 };
 
 import DiaryEntry from "../components/DiaryEntry";
 
 export default {
-  name: "Diary",
-  components: {
-    DiaryEntry
-  },
-  mounted() {
-    this.load();
-  },
-  data() {
-    return {
-      entries: []
-    };
-  },
-  methods: {
-    save() {
-      localStorage.entries = JSON.stringify(this.entries);
-    },
-    load() {
-      let storedEntries = localStorage.entries
-        ? JSON.parse(localStorage.entries)
-        : [];
-      this.entries = storedEntries.length
-        ? storedEntries
-        : [{ title: "", content: "", editing: true }];
-    },
-    entryToggledState(entry) {
-      if (entry.editing) return;
+	name: "Diary",
+	components: {
+		DiaryEntry
+	},
+	mounted() {
+		this.load();
+	},
+	data() {
+		return {
+			entries: []
+		};
+	},
+	methods: {
+		save() {
+			localStorage.entries = JSON.stringify(this.entries);
+		},
+		load() {
+			let storedEntries = localStorage.entries
+				? JSON.parse(localStorage.entries)
+				: [];
+			this.entries = storedEntries.length
+				? storedEntries
+				: [{ title: "", content: "", editing: true }];
+		},
+		entryToggledState(entry) {
+			if (entry.editing) return;
 
-      this.checkReplaceNewEntry();
-      this.save();
-    },
-    deleteEntry(index) {
-      this.entries.splice(index, 1);
+			this.checkReplaceNewEntry();
+			this.save();
+		},
+		deleteEntry(index) {
+			this.entries.splice(index, 1);
 
-      this.checkReplaceNewEntry();
-      this.save();
-    },
-    checkReplaceNewEntry() {
-      if (this.entries.every(e => !e.editing)) {
-        this.entries.splice(0, 0, {
-          title: "",
-          content: "",
-          editing: true
-        });
-      }
-    }
-  }
+			this.checkReplaceNewEntry();
+			this.save();
+		},
+		checkReplaceNewEntry() {
+			if (this.entries.every(e => !e.editing)) {
+				this.entries.splice(0, 0, {
+					title: "",
+					content: "",
+					editing: true
+				});
+			}
+		}
+	}
 };
 </script>
 
 <style scoped>
 .diary {
-  font-family: Arial, Helvetica, sans-serif;
-  height: 100%;
+	font-family: Arial, Helvetica, sans-serif;
+	height: 100%;
 }
 h1 {
-  font-family: monospace;
-  font-size: 24pt;
+	font-family: monospace;
+	font-size: 24pt;
 }
 .entriesWrapper {
-  display: flex;
-  justify-content: space-around;
+	display: flex;
+	justify-content: space-around;
 }
 .entries {
-  overflow-y: auto;
-  scroll-behavior: smooth;
-  display: flex;
-  flex-direction: column;
+	overflow-y: auto;
+	scroll-behavior: smooth;
+	display: flex;
+	flex-direction: column;
 }
 .entry {
-  margin: 8px;
-  flex-grow: 1;
+	margin: 8px;
+	flex-grow: 1;
 }
 </style>
